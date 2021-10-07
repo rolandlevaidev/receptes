5619d6e5a5be50f592b6b1463ad9a09a6d55be99
Roland Levai <70857984+aegnorwynn219@users.noreply.github.com>
2021-10-07 15:38:48 +0100

    dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import "./App.css";
+import { useState } from "react";
 import Recipe from "./components/Recipe";
+import RecipeSwitcher from "./components/recipeSwitcher";
+import RecipeList from "./assets/recipeList.json";
+import mealList from "./assets/mealList.json";
 
 function App() {
-  return <Recipe />;
+  const [chosenRecipe, setChosenRecipe] = useState("");
+
+  const changeRecipe = (event) => setChosenRecipe(event.target.value);
+
+  return (
+    <>
+      <RecipeSwitcher
+        chosenRecipe={chosenRecipe}
+        changeRecipe={changeRecipe}
+        mealList={mealList}
+      />
+      <Recipe recipe={RecipeList[chosenRecipe]} />
+    </>
+  );
 }
 
 export default App;
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,59 +1,48 @@
 import React from "react";
-import Clock from "Clock";
-import Fire from "Fire.js";
-import Scale from "Scale.js";
+import Clock from "./Clock";
+import Fire from "./Fire";
+import Scale from "./Scale";
 
-function Recipe() {
+function Recipe({ recipe }) {
+  if (!recipe) {
+    return <h1>Please choose a recipe... smh....</h1>;
+  }
   return (
     <>
-      <h2>Tejbegríz</h2>
-      <>
-        <div>
-          <>
-            <div>
-              <img src="" alt="Tejbegríz" />
-            </div>
-            <div className="recipeInfo">
-              <div className="recipeInfo">
-                <Clock />
-                <p>15 min</p>
-              </div>
-              <Scale
-                rating={2}
-                max={5}
-                fullIcon={() => <Fire isFull />}
-                emptyIcon={() => <Fire isFull={false} />}
-              />
-            </div>
-          </>
-        </div>
+      <h2 className="font-extrabold text-4xl">{recipe.title}</h2>
 
-        <div className="recipe">
-          <div className="ingredients">
-            <h3>Hozzávalók 3 főre</h3>
-            <ul className="ingredients_list">
-              <li>1l tej</li>
-              <li>120g búzadara</li>
-              <li>50g cukor</li>
-              <li>50g vaníliás cukor</li>
-              <li>25g vaj</li>
-              <li>cukor, kakaópor, fahéj</li>
-            </ul>
-          </div>
-          <div className="method">
-            <h3>Elkészítés</h3>
-            <p>
-              A tejben elkeverjük a búzadarát, a cukrot és a vaníliás cukrot, és
-              beledobjuk a vajat is, majd közepes lángon folyamatos keverés
-              mellett besűrítjük. Ha lágyabban szeretjük, adjunk hozzá még egy
-              kevés tejet, mert hűlés közben is sűrűsödik a tejbegríz. Mikor
-              szépen megszívták magukat a daraszemek, kimerjük a tejbegrízt 3-4
-              tálba, megszórjuk cukorral vagy porcukorral, és tetszőlegesen
-              fahéjjal vagy kakaóporral.
-            </p>
-          </div>
+      <div>
+        <div>
+          <img src={recipe.imgSrc} alt={recipe.title} />
         </div>
-      </>
+        <div className="recipeInfo">
+          <div className="recipeInfo">
+            <Clock />
+            <p>{recipe.time} min</p>
+          </div>
+          <Scale
+            rating={recipe.rating}
+            max={5}
+            fullIcon={() => <Fire isFull />}
+            emptyIcon={() => <Fire isFull={false} />}
+          />
+        </div>
+      </div>
+
+      <div className="recipe">
+        <div className="ingredients">
+          <h3>Hozzávalók 3 főre</h3>
+          <ul className="ingredients_list">
+            {recipe.ingredientsList.map((ingredient) => (
+              <li key={ingredient.trim().toLowerCase()}>{ingredient}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="method">
+          <h3>Elkészítés</h3>
+          <p>{recipe.description}</p>
+        </div>
+      </div>
     </>
   );
 }
